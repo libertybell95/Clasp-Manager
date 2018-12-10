24f59c35dbf409948e1246875bc4f4ceecff2b16
Joshua Bell <joshuakbell@gmail.com>
2018-12-10 12:40:30 -0600

    Updated comments

--- setupJSON.py
+++ setupJSON.py
@@ -3,7 +3,7 @@ import json
 import os
 
 data = {
-    "directory": os.path.dirname(os.path.realpath(__file__)), # Directory of parent folder
+    "directory": os.path.dirname(os.path.realpath(__file__)), # Directory of folder that this file is in
     "dbName": "dbTools.py" # Name of central database
 }
 
