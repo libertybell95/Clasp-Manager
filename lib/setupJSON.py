cc09b153f4e489b8d25bbe3c8e48cc1ee0625042
Joshua Bell <joshuakbell@gmail.com>
2018-12-10 14:05:59 -0600

    Optomizaiton of config.JSON.py

--- lib/setupJSON.py
+++ lib/setupJSON.py
@@ -1,12 +1,14 @@
 # Used to perform setup of JSON config file
 import json
 import os
-os.chdir("..") # Moves wokring directory out of lib folder into parent directory
+
+os.chdir(os.path.dirname(os.path.realpath(__file__))) ## Sets path to folder of file
 
 data = {
-    "directory": os.path.dirname(os.path.realpath(__file__)), # Directory of folder that this file is in
+    "directory": os.path.dirname(os.getcwd()), # Directory of program folder
     "dbName": "dbTools.py" # Name of central database
 }
 
+os.chdir("..")
 with open("config.json", "w") as outfile:
     json.dump(data, outfile, indent=4)
