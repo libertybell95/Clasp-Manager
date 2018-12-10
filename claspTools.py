9831fc248e5a8ad6c166837250de901edb1dd2d3
Joshua Bell <joshuakbell@gmail.com>
2018-12-10 12:55:49 -0600

    Reorganization & addint .clasp.json file maker for project folder

--- claspTools.py
+++ claspTools.py
@@ -6,30 +6,36 @@ import subprocess  # Used for calling/invoking CMD processes
 from dbTools import Tools
 
 class claspTools:
-        # def __init__(self):
-        #     self.test = 1
-
-        class dirTools:
-                def __init__(self):
-                        with open("config.json") as f: # Gets directory from config.JSON to determine starting point
-                                self.workPath = os.path.dirname(json.load(f)["directory"]+"\\projects\\")
+        def __init__(self):
+            self.test = 1
                 
-                def createFolder(self, name):
-                        """
-                        Create a folder in the projects directory with the following name.
+        def createFolder(self, name, scriptID):
+                """
+                Create a new projects folder with nessecary setup files
 
-                        :param name: {string} - Name of folder to be created
-                        """
-                        if not isinstance(name, str):
-                                raise ValueError("crateFolder(): String not entered for input.")
+                :param name: {string} - Name of folder to be created \n
+                :param name: {string} - scriptID to create .clasp.JSON file
+                """
+                if not isinstance(name, str):
+                        raise ValueError("createFolder(): String not entered for name.")
+                elif not isinstance(scriptID, str):
+                        raise ValueError("createFolder(): String not entered for scriptID.")
+                
+                with open("config.json") as f: # Gets directory from config.JSON to determine starting point
+                        workPath = os.path.dirname(json.load(f)["directory"]+"\\projects\\")
 
-                        os.chdir(self.workPath)
-                        newDir = "./" + name + "/"
-                        if not os.path.exists(newDir):
-                                os.makedirs(newDir)    
-                                print("Created new project folder" + newDir)
+                os.chdir(workPath)
+                newDir = "./" + name + "/"
+                if not os.path.exists(newDir):
+                        os.makedirs(newDir)    
+                        print("Created new project folder" + newDir)
+                        os.chdir(newDir)
 
-claspTools.dirTools().createFolder('')
+                with open(".clasp.json", "w") as f: # Sets up initial .clasp.json file
+                        data = {
+                                "scriptId": scriptID
+                        }
+                        json.dump(data, f, indent=4)
 
 # def getList():
 #     cmdOut = subprocess.check_output("clasp list", shell=True).decode("UTF-8") # Fetches raw console output of 'clasp list'
